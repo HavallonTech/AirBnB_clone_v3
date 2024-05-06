5329040417b45171d661528faee188ea7625b2ac
HavallonTech <kestercode@gmail.com>
2024-05-06 01:31:00 +0100

    ALX

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-"""Creating Flask app"""
-
-
-from api.v1.views import app_views
-from flask import Flask
+"""views"""
+from flask import Flask, Blueprint, jsonify
+from slask_cors import CORS
 from models import storage
+from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """ return 404 error """
+    errorF = {"error": "Not found"}
+    return jsonify(errorF), 404
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """close """
+    storage.close()
+
+
 if __name__ == "__main__":
-    HOST = getenv()
-    PORT = int(getenv())
-    app.run(host=HOST, prot=PORT, threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'), threaded=True)
